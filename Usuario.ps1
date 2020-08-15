00122583f6a8a5d205d2e3b0991686b1ac013040
Possamai <andreihpossamai@gmail.com>
2020-08-15 02:16:55 -0300

    Adicionando classe Usuario

--- Usuario.ps1
+++ Usuario.ps1
@@ -1,11 +1,40 @@
 ﻿class Usuario {
     
-    [string]$nome_completo
-    [string]$login
-    [string]$senha
-    [float]$carteira
+    [string]$_nome_completo
+    [string]$_login
+    [string]$_senha
+    [float]$_carteira
+    [array]$_operacoes
 
+    Usuario($nome_completo, $login, $senha, $carteira, $operacoes){
+        $this._nome_completo = $nome_completo
+        $this._login = $login
+        $this._senha = $senha
+        $this._carteira = $carteira
+        $this._operacoes = $operacoes
+    }
 
+    [string] get_nome_completo(){
+        return $this._nome_completo
+    }
 
+    [float] get_carteira(){
+        return $this._carteira
+    }
 
+    [array] get_operacoes(){
+        return $this._operacoes
+    }   
+
+    [void] add_operacao([object]$operacao){
+        $this._operacoes += $operacao
+    } 
+
+    [void] add_carteira([float]$valor){
+        $this._carteira += $valor
+    }
+
+    [string] to_string(){
+        return "Nome: " + $this.get_nome_completo() + " | Carteira: " + $this.get_carteira() + " | Operações: " + $this._operacoes.Count 
+    }
 }
