c57c46379c647b04baafa7532081c3322863b0fc
Possamai <andreihpossamai@gmail.com>
2020-08-18 19:44:48 -0300

    Ajustando gitignore

--- TesteClasses.ps1
+++ TesteClasses.ps1
@@ -1,7 +1,18 @@
 ﻿. .\Papel.ps1
+. .\Operacao.ps1
+. .\Usuario.ps1
 
-$papel = [Papel]::new("COGNA3", "Cogna Educação", 10.5)
+$papel = [Papel]::new('TEST4', 'Testes', 1.00)
+$operacao = [Operacao]::new($papel, 10, 10.00)
+$usuario = [Usuario]::new('teste', 'teste', 'teste', 0.00, @(0))
 
 $papel.to_string()
+$operacao.to_string()
+$usuario.to_string()
 
-#teste
+Write-Output "adicionando operação ao usuário"
+
+$usuario.add_operacao($operacao)
+
+$usuario.to_string()
+
